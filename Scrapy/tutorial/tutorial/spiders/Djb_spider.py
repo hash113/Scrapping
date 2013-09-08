80868a1da95377ab29c27d9937ce52ed06912b97
Hash113 <harshbhatia296@gmail.com>
2013-09-09 00:24:08 +0530

    commit of the day

--- Scrapy/tutorial/tutorial/spiders/Djb_spider.py
+++ Scrapy/tutorial/tutorial/spiders/Djb_spider.py
@@ -4,21 +4,24 @@ from scrapy.selector import HtmlXPathSelector
 from tutorial.items import DmozItem
 
 class DmozSpider(BaseSpider):
-   name = "dmoz"
-   allowed_domains = ["dmoz.org"]
-   start_urls = [
-       "http://www.djangobook.com/en/2.0/chapter02.html"
-   ]
+  name = "dmoz"
+  allowed_domains = ["dmoz.org"]
+  start_urls = [
+  "http://www.djangobook.com/en/2.0/chapter02.html"
+  ]
 
-   def parse(self, response):
-       hxs = HtmlXPathSelector(response)
-       sites = hxs.select('//pre')
-       items = []
-       for site in sites:
-           item = DmozItem()
-           #item['title'] = site.select('a/text()').extract()
-           #item['link'] = site.select('a/@href').extract()
-           #item['desc'] = site.select('text()').extract()
-           item['code'] = site.select('text()').extract()
-           items.append(item)
-       return items
+  def parse(self, response):
+    f=open('test.txt','w')
+    hxs = HtmlXPathSelector(response)
+    sites = hxs.select('//pre')
+    items = []
+    for site in sites:
+      item = DmozItem()
+      item['title'] = site.select('a/text()').extract()
+      item['link'] = site.select('a/@href').extract()
+      item['desc'] = site.select('text()').extract()
+      item['code'] = site.select('text()').extract()
+      items.append(item)
+    print "heere i am " 
+    print items
+    return items
