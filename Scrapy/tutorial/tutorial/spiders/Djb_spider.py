3f13b11624f260645a883498a2ae7e639aea0412
Hash113 <harshbhatia296@gmail.com>
2013-09-09 10:50:31 +0530

    description extracted

--- Scrapy/tutorial/tutorial/spiders/Djb_spider.py
+++ Scrapy/tutorial/tutorial/spiders/Djb_spider.py
@@ -4,24 +4,25 @@ from scrapy.selector import HtmlXPathSelector
 from tutorial.items import DmozItem
 
 class DmozSpider(BaseSpider):
-  name = "dmoz"
-  allowed_domains = ["dmoz.org"]
+  name = "dmoz1"
+  allowed_domains = ["thedjangobook.com"]
   start_urls = [
   "http://www.djangobook.com/en/2.0/chapter02.html"
   ]
 
   def parse(self, response):
-    f=open('test.txt','w')
     hxs = HtmlXPathSelector(response)
     sites = hxs.select('//pre')
     items = []
     for site in sites:
       item = DmozItem()
-      item['title'] = site.select('a/text()').extract()
-      item['link'] = site.select('a/@href').extract()
-      item['desc'] = site.select('text()').extract()
-      item['code'] = site.select('text()').extract()
+      #item['title'] = site.select('a/text()').extract()
+       #item['link'] = site.select('a/@href').extract()
+       #item['desc'] = site.select('text()').extract()
+      item['code'] = site.select('a/text()').extract()
       items.append(item)
-    print "heere i am " 
-    print items
+      f=open('test.txt','w')
+      f.write(items)     
     return items
+
+
